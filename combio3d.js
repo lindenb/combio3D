9201118dcfa6624d1a1a138b52a28665950e91f4
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2015-05-18 22:31:09 +0200

    cont

--- combio3d.js
+++ combio3d.js
@@ -38,13 +38,13 @@ Combio.prototype.square=function(points,normal)
 	{
 	f= this.facet(points,normal);
 	f+= this.facet([
-		points[0],
+		points[2],
 			[
 			points[2][0] + (points[0][0] - points[1][0]), 
 			points[2][1] + (points[0][1] - points[1][1]), 
 			points[2][2] + (points[0][2] - points[1][2])
 			],
-		points[2]
+		points[0]
 		],normal);
 	return f;
 	};
