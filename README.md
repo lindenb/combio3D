113a91b052f317672cd44fd90f20131dcca531ab
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2015-05-18 22:20:46 +0200

    works !

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # combio3D
-test STL
 
-# See also
+Test STL File format.
+
+## Author
+
+* Pierre Lindenbaum PhD @yokofakun
+
+## See also
 
 * http://en.wikipedia.org/wiki/STL_%28file_format%29
 * http://www.ennex.com/~fabbers/StL.asp
+* http://www.viewstl.com/
+
+
+## Contribute
+
+- Issue Tracker: http://github.com/lindenb/combio3d/issues`
+- Source Code: http://github.com/lindenb/combio3d
+
+##History
+
+* 2015 : Creation
+
+## License
+
+The project is licensed under the MIT license.
+
+
--- combio3d.js
+++ combio3d.js
@@ -1,91 +1,204 @@
-function $(id)
+function Combio()
 	{
-	return document.getElementById(id);
+	
 	}
 
-function intgtzero(id)
+
+
+
+Combio.prototype.intValue=function(id)
 	{
-	var input = $(id);
+	var input = document.getElementById(id);
 	var n = parseInt(input.value, 10);
 	if (Number( input.value)=== n ) return n;
 	throw "Bad Integer for "+id;
-	}
+	};
 	
-function vertex(point)
+Combio.prototype.vertex=function(point)
 	{
-	return "   vertex "+point.x+" "+point.y+" "+point.z+"\n";
-	}	
+	return "   vertex "+point[0]+" "+point[1]+" "+point[2]+"\n";
+	};
 	
 
-function facet(points)
+Combio.prototype.facet=function(points,normal)
 	{
 	var i,f="";
-	f+=" facet normal 0 -1 0\n";
+	f+=" facet normal "+normal[0]+" "+normal[1]+" "+normal[2]+"\n";
 	f+="  outer loop\n";
 	for(i=0;i< 3;++i)
 		{
-		f+= vertex(points[i]);
+		f+= this.vertex(points[i]);
 		}
 	f+="  endloop\n";
 	f+=" endfacet\n";
 	return f;
-	}
+	};
 
-function square(points)
+Combio.prototype.square=function(points,normal)
 	{
-	f= facet(points);
-	f+= facet([
+	f= this.facet(points,normal);
+	f+= this.facet([
 		points[0],
-			{
-			x: points[2].x + (points[0].x - points[1].x), 
-			y: points[2].y + (points[0].y - points[1].y), 
-			z: points[2].z + (points[0].z - points[1].z)
-			},
+			[
+			points[2][0] + (points[0][0] - points[1][0]), 
+			points[2][1] + (points[0][1] - points[1][1]), 
+			points[2][2] + (points[0][2] - points[1][2])
+			],
 		points[2]
-		]);
+		],normal);
 	return f;
-	}
+	};
 
-function cub(points,height)
+Combio.prototype.repaint=function(evt)
 	{
-	//top face
-	f=  square(points);
-	//bottom face
-	f+= square([
-		{x:points[0].x,y:points[0].y,z:points[0].z - height},
-		{x:points[0].x,y:points[0].y,z:points[0].z - height},
-		{x:points[0].x,y:points[0].y,z:points[0].z - height}
-		]);
-	return f;
-	}
-
-function update_stl(evt)
-	{
-	var i,stl="solid comb\n", pre = $("output");
-	while(pre.hasChildNodes()) pre.removeChild( pre.firstChild );
+	var i,path="",x=0,y=0,width=0,height=0;
 	try
 		{
-		var  n = intgtzero("N");
-		for(i=0;i<n;++i)
+		var  nTeeth = this.intValue("nTeeth");
+		for(i=0; i < nTeeth;++i)
 			{
-			stl+= square([{x:1,y:2,z:3},{x:1,y:-2,z:3},{x:10,y:2,z:23}]);
+			//stl+= this.square([[1,2,3],[4,5,6],[4,5,6]]);
 			}
-		stl += "endsolid comb\n";
+		
+		path  ="0,0";
+		y+=this.intValue("a") ; path += " "+x+","+y;
+		x+=this.intValue("b") ; path += " "+x+","+y;
+		y+=this.intValue("c") ; path += " "+x+","+y;
+		height = y;
+		for(i = 0;i< nTeeth;++i)
+			{
+			y+=this.intValue("d") ; path += " "+x+","+y; height = y;
+			x+=this.intValue("e") ; path += " "+x+","+y;
+			y-=this.intValue("d") ; path += " "+x+","+y;
+			if(i+1<nTeeth)
+				{
+				x+=this.intValue("f") ; path += " "+x+","+y;
+				}
+			}
+		y-=this.intValue("c") ; path += " "+x+","+y;
+		x+=this.intValue("b") ; path += " "+x+","+y; width=x;
+		y-=this.intValue("a") ; path += " "+x+","+y;
+		path += " 0,0";
+		width+=this.intValue("thickness")+1;
+		height+=this.intValue("thickness")+1;
+		document.getElementById("transform").setAttribute(
+			"transform","translate("+this.intValue("thickness")+","+this.intValue("thickness")+")");
+		document.getElementById("path0").setAttribute("points",path);
+		document.getElementById("path1").setAttribute("points",path);
+		document.getElementById("image").setAttribute("width",width);
+		document.getElementById("image").setAttribute("height",height);
 		}
 	catch(err)
 		{
-		stl="#Error "+ err.message;
+		console.log(err.message);
+		}
+
+	};
+
+Combio.prototype.cubeface=function(x,y,width,height)
+	{
+	var s="";
+	var z=this.intValue("thickness");
+	// face I
+	s+= this.square([
+		[x,y+height,0],
+		[x,y,0],
+		[x+width,y,0],
+		],[0,0,-1]);
+	
+	//face VI
+	s+= this.square([
+		[x,y+height,z],
+		[x,y,z],
+		[x+width,y,z],
+		],[0,0,1]);
+	
+	//face II
+	s+= this.square([
+		[x,y,0],
+		[x,y,z],
+		[x,y+height,z],
+		],[-1,0,0]);
+	
+	//face IV
+	s+= this.square([
+		[x+width,y,0],
+		[x+width,y,z],
+		[x+width,y+height,z],
+		],[1,0,0]);
+	
+	// face I
+	s+= this.square([
+		[x,y,0],
+		[x,y,z],
+		[x+width,y,z],
+		],[0,-1,0]);
+	// face IV
+	s+= this.square([
+		[x,y+height,0],
+		[x,y+height,z],
+		[x+width,y+height,z],
+		],[0,1,0]);
+	
+	return s;
+	};
+
+Combio.prototype.update=function(evt)
+	{
+	var i,stl="solid comb\n",
+		pre = document.getElementById("output");
+	
+	
+	while(pre.hasChildNodes()) pre.removeChild( pre.firstChild );
+	try
+		{
+		var  nTeeth = this.intValue("nTeeth");
+		stl += this.cubeface(0,0,
+				2*this.intValue("b") + nTeeth*this.intValue("e") + (nTeeth-1)*this.intValue("f"),
+				this.intValue("a")
+				);
+		
+		stl += this.cubeface(
+				this.intValue("b"),
+				this.intValue("a"),
+				nTeeth*this.intValue("e") + (nTeeth-1)*this.intValue("f"),
+				this.intValue("c")
+				);
+		
+		for(i=0; i < nTeeth;++i)
+			{
+			stl += this.cubeface(
+				this.intValue("b")+i*(this.intValue("e")+this.intValue("f")),
+				this.intValue("a")+this.intValue("c"),
+				this.intValue("e"),
+				this.intValue("d")
+				);
+			}
+		stl += "endsolid comb\n";
+		this.repaint();
+		}
+	catch(err)
+		{
+		stl="#Error ("+ err.message+")";
 		}
 	pre.appendChild(document.createTextNode(stl));
-	}
+	};
+	
+
 
 window.addEventListener("load",function()
 	{
+	var main = new Combio();
 	var i,inputs = document.getElementsByTagName("input");
-	
+	var callback = function(evt)
+		{
+		
+		main.update();
+		};
+	main.repaint();
 	for(i=0;i < inputs.length;++i)
 		{
-		inputs[i].addEventListener("change",update_stl,false);
-		}   
+		inputs[i].addEventListener("change",callback,false);
+		}
 	},false);
 
